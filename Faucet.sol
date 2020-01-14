82c3501fdaa08424490e1b2fd76d72af7cb035cd
skorobogatovamail <39035661+skorobogatovamail@users.noreply.github.com>
2020-01-14 21:55:43 +0300

    Update Faucet.sol

--- Faucet.sol
+++ Faucet.sol
@@ -17,7 +17,7 @@ contract Faucet {
     }
     
     function getCoin(uint256 amount) public {
-        require( address(this).balance >=  amount, "sorry, there amount of tolens available is less than your request");
+        require( address(this).balance >=  amount, "sorry, the amount of tokens available is less than your request");
         if (msg.sender != owner) {
             require(amount <= 0.1 ether, "sorry, you can get no more than 0.1 ether");
             require(didRecieve[msg.sender] == false, "sorry, you can use this function only once");
